fd5a6777a2cb45403ec84718299022f6bbb4233b
vagrant <vagrant@homestead>
2018-05-23 03:39:58 +0000

    增加编辑页面的身份验证策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,11 +16,15 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
+        
         $data = $request->all();
 
         if ($request->avatar) {
@@ -32,5 +36,10 @@ class UsersController extends Controller
 
         $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
     }
 }
