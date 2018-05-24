4a3f7c9f468c14ffc9a16940f33081b2f8f723d9
vagrant <vagrant@homestead>
2018-05-24 03:18:47 +0000

    用户只能编辑自己的帖子策略

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -43,11 +43,13 @@ class TopicsController extends Controller
         return redirect()->route('topics.show', $topic->id)->with('success', '话题发布成功');
     }
 
-	public function edit(Topic $topic)
-	{
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
+        $categories = Category::all();
+        
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
 	public function update(TopicRequest $request, Topic $topic)
 	{
