5db62268246baa5d2c71ecfbca8f328715810ac6
vagrant <vagrant@homestead>
2018-05-24 03:27:40 +0000

    删除策咯

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -47,7 +47,7 @@ class TopicsController extends Controller
     {
         $this->authorize('update', $topic);
         $categories = Category::all();
-        
+
         return view('topics.create_and_edit', compact('topic', 'categories'));
     }
 
