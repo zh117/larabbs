e6526a2359ca0dea6e515172619336ad0a923fe0
vagrant <vagrant@homestead>
2018-05-26 08:58:53 +0000

    测试

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -16,6 +16,7 @@ class Link extends Model
     {
         // 尝试从缓存中取出 cache_key 对应的数据。如果能取到，便直接返回数据。
         // 否则运行匿名函数中的代码来取出 links 表中所有的数据，返回的同时做了缓存。
+        dd($this->all());
         return Cache::remember($this->cache_key, $this->cache_expire_in_minutes, function(){
             return $this->all();
         });
